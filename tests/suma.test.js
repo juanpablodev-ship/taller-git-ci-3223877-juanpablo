0c3eb43951917f02180d3623eebfde92224a3f36
juanSA26273 <juanpablosernaarboleda464@gmail.com>
2025-12-02 11:33:20 -0500

    style: aplicar formato con prettier

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,8 @@
-const suma = require('../src/app');
-
-test('suma de 1 + 2 es 3', () => {
-  expect(suma(1, 2)).toBe(3);
+import test from 'node:test';
+import assert from 'node:assert/strict';
+function suma(a, b) {
+  return a + b;
+}
+test('suma 2 + 3 = 5', () => {
+  assert.equal(suma(2, 3), 5);
 });
