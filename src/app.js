c8f26ac23013eeec50dbe8074fdf2ced683037d0
juanSA26273 <juanpablosernaarboleda464@gmail.com>
2025-12-02 10:40:46 -0500

    style: aplicar formato con prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 function agregarItem(texto) {
-  const lista = document.getElementById("lista");
-  const li = document.createElement("li");
+  const lista = document.getElementById('lista');
+  const li = document.createElement('li');
   li.textContent = texto;
   lista.appendChild(li);
 }
 
-console.log("App lista");
+console.log('App lista');
--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,5 +1,5 @@
 const suma = require('../src/app');
 
 test('suma de 1 + 2 es 3', () => {
-    expect(suma(1, 2)).toBe(3);
+  expect(suma(1, 2)).toBe(3);
 });
