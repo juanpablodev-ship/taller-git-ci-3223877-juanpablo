d53a9829c0103da51b89ce21654222f91c2a0b97
juanSA26273 <juanpablosernaarboleda464@gmail.com>
2025-12-02 10:05:13 -0500

    feat(app): crear función agregarItem

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-function suma(a, b) {
-    return a + b;
+function agregarItem(texto) {
+  const lista = document.getElementById("lista");
+  const li = document.createElement("li");
+  li.textContent = texto;
+  lista.appendChild(li);
 }
 
-module.exports = suma;
+console.log("App lista");
